9ce16141f787d796b84d9be879debbf12838f29f
coding-ui <clashsathishnon@gmail.com>
2023-10-08 20:42:37 +0530

    author check,changed variable to a.

--- java_tns_c2c/src/tns1_pacakage/variable1.java
+++ java_tns_c2c/src/tns1_pacakage/variable1.java
@@ -4,8 +4,9 @@ public class variable1 {
 
 	public static void main(String[] args) {
 		// printing variable
-		int x=15;
-		System.out.println(x);
+		//just for author check
+		int a=15;
+		System.out.println(a);
 		int y=14;
 		System.out.println(y);
 
