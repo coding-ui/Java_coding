3200051d7aabcd7b9a618663cda5f132e3d1d3b0
coding-ui <clashsathishnon@gmail.com>
2023-10-08 20:51:42 +0530

    new changes.

--- java_tns_c2c/src/tns1_pacakage/variable1.java
+++ java_tns_c2c/src/tns1_pacakage/variable1.java
@@ -6,7 +6,7 @@ public class variable1 {
 		// printing variable
 		//just for author check
 		int a=15;
-		System.out.println(a);
+		System.out.println(a,"a");
 		int y=14;
 		System.out.println(y); 
 
